ede0b7659fa540228ebd8fe91c11a3fb9532d5cf
liu245933567 <245933567@qq.com>
2022-05-10 11:01:25 +0800

    chore: 添加断点调试

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { NestLogsModule } from 'nest-logs';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BlogModule } from './blog/blog.module';
 
 @Module({
-  imports: [],
+  imports: [
+    NestLogsModule,
+    MongooseModule.forRoot('mongodb://localhost/nest-blog', {
+      useNewUrlParser: true,
+    }),
+    BlogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
